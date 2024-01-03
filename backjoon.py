75f82621a140c83a4c9b1adcbe9ed92918c36f63
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-01-03 13:01:01 +0900

    test

---
+++ backjoon.py
@@ -0,0 +1,15 @@
+import sys
+#sys.stdin=open("test.txt")
+
+n = map(int, input().split())
+
+a = list(map(int, sys.stdin.readline().split())) #6 9 5 7 4
+b = list(map(int, sys.stdin.readline().split())) #6 9 5 7 4
+
+total_list = a + b
+total_list.sort()
+
+answer = ' '.join(map(str, total_list))
+print(answer)
+
+
