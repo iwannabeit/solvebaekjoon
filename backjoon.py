fa4cd769d07c86c96f814dca08f63c90691cc811
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-01-15 23:37:37 +0900

    bj #2231

--- backjoon.py
+++ backjoon.py
@@ -1,14 +1,15 @@
-import sys
-#sys.stdin=open("test.txt")
+from sys import stdin
 
-n = map(int, input().split())
+n = int(stdin.readline())
 
-a = list(map(int, sys.stdin.readline().split())) #6 9 5 7 4
-b = list(map(int, sys.stdin.readline().split())) #6 9 5 7 4
-
-total_list = a + b
-total_list.sort()
-
-answer = ' '.join(map(str, total_list))
-print(answer)
-
+for i in range(1, n+1):
+  result = 0
+  num_list = list(map(int, str(i)))
+  for j in num_list:
+    result += int(j)
+  check = result + i
+  if check == n:
+    print(i)
+    break
+else:
+  print(0)
