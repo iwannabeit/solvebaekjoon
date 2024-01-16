61947653b19e69a8bdd28f0ac59e691e002588f0
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-01-17 03:29:23 +0900

    bj #2798 : 블랙잭

--- backjoon.py
+++ backjoon.py
@@ -1,15 +1,16 @@
 from sys import stdin
+from itertools import combinations
 
-n = int(stdin.readline())
 
-for i in range(1, n+1):
-  result = 0
-  num_list = list(map(int, str(i)))
-  for j in num_list:
-    result += int(j)
-  check = result + i
-  if check == n:
-    print(i)
-    break
-else:
-  print(0)
+n, m = map(int, input().split())
+card = list(map(int, input().split()))
+result = 0
+
+all_combi = list(combinations(card, 3))
+
+for i in range(len(all_combi)):
+  cb_sum = sum(all_combi[i])
+  if cb_sum <= m and cb_sum >= result:
+    result = cb_sum
+
+print(result)
