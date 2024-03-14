f9c34a2bf1c5c70145943d40c55b97d3277bf1d4
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-03-14 21:57:03 +0900

    bj #1541 : 잃어버린 괄호

--- backjoon.py
+++ backjoon.py
@@ -1,16 +1,19 @@
 from sys import stdin
-from itertools import combinations
 
+exq = stdin.readline()
+ans = 0
 
-n, m = map(int, input().split())
-card = list(map(int, input().split()))
-result = 0
+minus = exq.split('-')
 
-all_combi = list(combinations(card, 3))
+# 첫번째 수가 양수인 경우를 대비해서 처리해줌
+# 음수인 경우는 0번째 인덱스에 ''가 들어감.
+for i in minus[0].split('+'):
+    ans += int(i)
 
-for i in range(len(all_combi)):
-  cb_sum = sum(all_combi[i])
-  if cb_sum <= m and cb_sum >= result:
-    result = cb_sum
-
-print(result)
+# 1번째 인덱스 부터는 다음 -가 오기 전에 빼주면 됨.
+for i in minus[1:]:
+    next = i.split('+')
+    for j in next:
+        ans -= int(j)
+        
+print(ans)
