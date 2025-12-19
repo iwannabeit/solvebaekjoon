6282ec5a5f15685520f0f75933696bc8aaf3f009
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2025-12-19 10:56:26 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,32 +2,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-    public int solution(int n, int[][] computers) {
-        int answer = n;
-        Queue<Integer> q = new LinkedList<>();
+    static int answer = 0;
+    
+    static int solution(int n, int[][] computers){
         boolean[] visited = new boolean[computers.length];
-
-        for(int start = 0; start<computers.length;start++){
+        answer = computers.length;
+        for(int start=0; start<computers.length; start++){
             if(visited[start]) continue;
-            
-            q.offer(start);
-            
-            while(!q.isEmpty()){
-                int nodeIdx = q.poll();
-                visited[nodeIdx] = true;
-                
-                for(int i=0; i<computers[nodeIdx].length; i++){
-                    if(computers[nodeIdx][i] == 1 && !visited[i]){
-                        visited[i] = true;
-                        answer -= 1;
-                        q.offer(i);
-                    }
-                }
-                
-            }
-                
+            dfs(start, visited, computers);
         }
-
         return answer;
+    }
+
+    static void dfs(int idx, boolean[] visited, int[][] computers){
+        visited[idx] = true;
+
+        for(int i=0; i<computers[idx].length; i++){
+            if(computers[idx][i] == 1 && !visited[i]){
+                answer -= 1;
+                dfs(i, visited, computers);
+            }
+        }
     }
 }
